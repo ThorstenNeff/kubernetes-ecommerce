caae4074d945c6c0a82ffe362efb3412d1c354fa
thorstenneff <thorstenneff@gmx.de>
2024-12-13 16:50:58 +0100

    Add NotFound error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express(); 
 app.use(json());
@@ -13,6 +14,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.get('*', () => {
+    console.log("In not found");
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, (() => {
