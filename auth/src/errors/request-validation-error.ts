fda9b7afa688e6f920176d8ce54b6adffd76b73f
thorstenneff <thorstenneff@gmx.de>
2024-12-13 16:40:37 +0100

    Add abstract CustomError class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,10 @@
 import { ValidationError } from 'express-validator';
-
-export class RequestValidationError extends Error {
+import { CustomError } from './custom-error';
+export class RequestValidationError extends CustomError {
     statusCode = 400; 
 
     constructor(public errors: ValidationError[]) {
-        super();
+        super("Invalid signup parameters"); 
 
         // Only because we are extending a built-in class
         Object.setPrototypeOf(this, RequestValidationError.prototype);
@@ -12,7 +12,11 @@ export class RequestValidationError extends Error {
 
     serializeErrors() {
         return this.errors.map((error) => {
-            return { message: error.msg, field: error.path };
+            if (error.type === 'field') {
+                return { message: error.msg, field: error.path };
+            } else {
+                return { message: error.msg }
+            }
         });
     }
 }
