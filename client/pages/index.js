f1c26045e2fa46e6e196faac75cbd71de8ee170f
thorstenneff <thorstenneff@gmx.de>
2024-12-14 19:29:45 +0100

    Show login status on landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,11 @@ import buildClient from "../api/build-client"
 
 const LandingPage = ({currentUser}) => {
   console.log("Current user: ", currentUser);
-  return <h1>Landing Page</h1>;
+  if (currentUser) {
+    return <h1>You are signed in</h1>;
+  } else {
+    return <h1>You are not signed in</h1>;
+  }
 };
   
 LandingPage.getInitialProps = async (context) => {
