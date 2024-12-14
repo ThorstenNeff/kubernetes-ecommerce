2e34b992864db4d374da2e4e2b61ea908989ab9c
thorstenneff <thorstenneff@gmx.de>
2024-12-14 19:24:10 +0100

    Add buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,24 +1,13 @@
-import axios from 'axios';
+import buildClient from "../api/build-client"
 
 const LandingPage = ({currentUser}) => {
   console.log("Current user: ", currentUser);
   return <h1>Landing Page</h1>;
 };
   
-LandingPage.getInitialProps = async ({req}) => {
-  if (typeof window === 'undefined') {
-    console.log("getInitialProps on the server");
-    const response = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
-      {
-        headers: req.headers
-      })
-    return response.data;
-  } else {
-    console.log("getInitialProps on the browser");
-    const response = await axios.get('/api/users/currentuser');
-    return response.data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const {data} = await buildClient(context).get('/api/users/currentuser');
+  return data; 
 };
 
 export default LandingPage;
