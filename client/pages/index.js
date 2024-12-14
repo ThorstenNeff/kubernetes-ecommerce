dd71c3f00ce3735f42fdf0b4fc1de76c71d2a962
thorstenneff <thorstenneff@gmx.de>
2024-12-14 17:43:10 +0100

    getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,11 @@
-const Landing = () => {
-    return <h1>Landing Page</h1>;
-  };
-   
-  export default Landing;
+const LandingPage = ({color}) => {
+  console.log("I am in the component ", color);
+  return <h1>Landing Page</h1>;
+};
+  
+LandingPage.getInitialProps = () => {
+  console.log("I am on the server");
+  return { color: 'red' };
+};
+
+export default LandingPage;
