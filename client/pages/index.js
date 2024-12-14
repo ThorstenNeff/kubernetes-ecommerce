0444a1a4fa9b0e0a0304886406b9bba51b91960b
thorstenneff <thorstenneff@gmx.de>
2024-12-14 19:09:13 +0100

    Load user via ingress in getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,24 @@
-const LandingPage = ({color}) => {
-  console.log("I am in the component ", color);
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+  console.log("Current user: ", currentUser);
   return <h1>Landing Page</h1>;
 };
   
-LandingPage.getInitialProps = () => {
-  console.log("I am on the server");
-  return { color: 'red' };
+LandingPage.getInitialProps = async ({req}) => {
+  if (typeof window === 'undefined') {
+    console.log("getInitialProps on the server");
+    const response = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+      {
+        headers: req.headers
+      })
+    return response.data;
+  } else {
+    console.log("getInitialProps on the browser");
+    const response = await axios.get('/api/users/currentuser');
+    return response.data;
+  }
 };
 
 export default LandingPage;
