f969fd14c2c806203790ae430a8491374b0ec564
thorstenneff <thorstenneff@gmx.de>
2024-12-14 13:45:14 +0100

    Add signup test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,8 @@ import { app } from '../app';
 let mongo: any; 
 
 beforeAll(async () => {
+    process.env.JWT_KEY = "key_for_tests";
+
     mongo = await MongoMemoryServer.create();
     const mongoUri = mongo.getUri();
 
