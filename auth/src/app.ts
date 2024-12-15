0b54e99120c2d8ff68b69490d0cfcb38562dce1c
thorstenneff <thorstenneff@gmx.de>
2024-12-15 14:30:49 +0100

    Fix auth

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,7 +7,7 @@ import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
+import { errorHandler } from '@neffuke/common'
 import { NotFoundError } from '../../common/src/errors/not-found-error'
 
 const app = express(); 
