0f9bfdb2573aace9a7d1fabfe105d0fabb289f6a
thorstenneff <thorstenneff@gmx.de>
2024-12-15 14:15:34 +0100

    Update version

--- common/src/index.ts
+++ common/src/index.ts
@@ -8,3 +8,5 @@ export * from "./middlewares/current-user"
 export * from "./middlewares/error-handler"
 export * from "./middlewares/require-auth"
 export * from "./middlewares/validate-request"
+
+console.log("Test");
