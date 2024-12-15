8d087e293526240d80af013da10fef2b333008f3
thorstenneff <thorstenneff@gmx.de>
2024-12-15 11:25:20 +0100

    Pass through getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
 // Overwrite default Next.js app.js file (> thin wrapper around Next screens)
 // The current next screen is in Component
 
-export default ({ Component, pageProps}) => {
-    return <Component {...pageProps} />
+const AppComponent =  ({ Component, pageProps, currentUser}) => {
+    return (
+        <div>
+            <h1>Ticketing {currentUser.email}</h1>
+            <Component {...pageProps} />
+        </div>
+    );
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentuser');
+    
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    return {
+        pageProps,
+        currentUser: data.currentUser
+    };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -10,6 +10,7 @@ const LandingPage = ({currentUser}) => {
 };
   
 LandingPage.getInitialProps = async (context) => {
+  console.log("LANDING PAGE")
   const {data} = await buildClient(context).get('/api/users/currentuser');
   return data; 
 };
