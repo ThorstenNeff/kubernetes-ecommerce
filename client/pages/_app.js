4b04742960ffc0b22a54e4ef3e1cd13973c4db89
thorstenneff <thorstenneff@gmx.de>
2024-12-15 12:16:06 +0100

    Add header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
-
+import Link from 'next/link';
+import AppHeader from '../appcomponents/app-header';
 // Overwrite default Next.js app.js file (> thin wrapper around Next screens)
 // The current next screen is in Component
 
 const AppComponent =  ({ Component, pageProps, currentUser}) => {
+    const links = [
+        !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+        !currentUser && { label: 'Sign In', href: '/auth/signin' },
+        currentUser && { label: 'Sign Out', href: '/auth/signout' },
+      ]
+        .filter((linkConfig) => linkConfig)
+        .map(({ label, href }) => {
+          return (
+            <li key={href} className="nav-item">
+              <Link className="nav-link" href={href}>
+                {label}
+              </Link>
+            </li>
+          );
+        });
+
     return (
         <div>
-            <h1>Ticketing {currentUser.email}</h1>
+            <AppHeader currentUser={currentUser}></AppHeader>
             <Component {...pageProps} />
         </div>
     );
