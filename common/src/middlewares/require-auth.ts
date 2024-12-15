277acb568866856ad46293322977b3ddbdbad4c6
thorstenneff <thorstenneff@gmx.de>
2024-12-15 14:04:02 +0100

    Move errors and middlewares to common module

---
+++ common/src/middlewares/require-auth.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from '../../../common/src/errors/not-authorized-error';
+
+export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.currentUser) {
+        throw new NotAuthorizedError();
+    }
+
+    next(); 
+};
