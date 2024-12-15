8ce9cf6b062e47ee0dd8fb7eaf1c997dcbb66bfb
thorstenneff <thorstenneff@gmx.de>
2024-12-15 16:43:03 +0100

    Add signin method for ticket tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 declare global {
-    var signin: () => Promise<string[]>;
+    var signin: () => string[];
 }
 
 let mongo: any; 
@@ -35,18 +36,14 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = "test@test.com";
-    const password = "password";
-
-    const response = await request(app)
-        .post('/api/users/signup')
-        .send({email,password})
-        .expect(201)
-
-    const cookie = response.get('Set-Cookie');
-    if (!cookie) {
-        throw new Error("Failed to get cookie from response");
-      }
-    return cookie; 
+global.signin = () => {
+    const payload = {
+        id: '1234',
+        email: 'test@test.com'
+    };
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+    const session = {jwt: token};
+    const sessionJson = JSON.stringify(session);
+    const base64 = Buffer.from(sessionJson).toString('base64');
+    return [`session=${base64}`];
 }
