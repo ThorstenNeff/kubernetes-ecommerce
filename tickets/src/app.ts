b1a3aa6d45cb927474ddc7f4d58b86d20465dd64
thorstenneff <thorstenneff@gmx.de>
2024-12-16 23:35:40 +0100

    Add update route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,7 +5,8 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@neffuke/common'
 import { createTicketRouter  } from './routes/new';
 import { showTicketRouter  } from './routes/show';
-import { indexTicketRouter } from './routes/index'
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express(); 
 app.set('trust proxy', true);
@@ -20,6 +21,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
     console.log("In not found");
