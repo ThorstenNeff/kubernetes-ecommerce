d7059cc772530f62d51767b97eb10aa5268e9aaf
thorstenneff <thorstenneff@gmx.de>
2024-12-16 23:04:52 +0100

    Add index route

---
+++ tickets/src/routes/__test__/index.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+const createTicket = () => {
+    return request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: "1",
+      price: 100,
+    })
+}
+
+it('can fetch a list of tickets', async () => {
+  await createTicket();
+  await createTicket();
+  await createTicket();
+
+  const response = await request(app).get('/api/tickets').send({});
+  expect(response.status).toEqual(200);
+  expect (response.body.length).toEqual(3);
+});
