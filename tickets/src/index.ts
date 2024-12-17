748e79fea140711148c4353df5c02a3c0c8e61ec
thorstenneff <thorstenneff@gmx.de>
2024-12-17 16:41:03 +0100

    NATS streaming service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,7 +12,7 @@ const start = async () => {
     }
 
     try {
-        await mongoose.connect('process.env.MONGO_URI',  {
+        await mongoose.connect(process.env.MONGO_URI,  {
         } as ConnectOptions);
         console.log("Connected to MongoDB");
     } catch (err) {
