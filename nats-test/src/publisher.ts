99653487cf5a044d7e2e494d69b561b2d6858317
thorstenneff <thorstenneff@gmx.de>
2025-01-06 18:12:06 +0100

    Refactor publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,7 @@
 import nats from 'node-nats-streaming'
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+import { TicketCreatedEvent } from './events/ticket-created-event';
+import { Subjects } from './events/subjects';
 
 console.clear();
 
@@ -8,14 +11,11 @@ const stan = nats.connect('docker-desktop', 'abc', {
 
 stan.on('connect', () => {
     console.log("Publisher connected to NATS");
+    const publisher = new TicketCreatedPublisher(stan);
 
-    const data = JSON.stringify({
+    publisher.publish({
         id: '123',
         title: 'concert',
         price: 20
-    });
-
-    stan.publish('ticket:created', data, () => {
-        console.log("Event published");
     });
 });
