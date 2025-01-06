759d250f9a8504ab04f79391b030e44ae7b07e09
thorstenneff <thorstenneff@gmx.de>
2025-01-06 17:38:40 +0100

    Refactor Listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message, SubscriptionOptions, Stan } from 'node-nats-streaming'
+import nats from 'node-nats-streaming'
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener'
 
 console.clear();
 
@@ -19,57 +20,3 @@ stan.on('connect', () => {
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-
-abstract class Listener {
-    abstract subject: string;
-    abstract queueGoupName: string;
-    abstract onMessage(data: any, msg: Message): void;
-    private client: Stan;
-    protected ackWait = 5 * 1000; 
-
-    constructor(client: Stan) {
-        this.client = client;
-    }
-
-    subscriptionOptions() {
-        return this.client
-        .subscriptionOptions()
-        .setDeliverAllAvailable()
-        .setManualAckMode(true)
-        .setAckWait(this.ackWait)
-        .setDurableName(this.queueGoupName);
-    }
-
-    listen() {
-        console.log("Listen");
-        const subscription = stan.subscribe(
-            this.subject,
-            this.queueGoupName, 
-            this.subscriptionOptions()
-        );
-
-        subscription.on('message', (msg: Message) => {
-            console.log(
-                `Message received: ${this.subject} ${this.queueGoupName}`
-            )
-            const parsedData = this.parseMessage(msg);
-            this.onMessage(parsedData, msg)
-        });
-    }
-
-    parseMessage(msg: Message) {
-        const data = msg.getData();
-        return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf8'));
-    }
-}
-
-class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
-    queueGoupName = 'payments-service';
-
-    onMessage(data: any, msg: Message) {
-        console.log('Event data', data);
-
-        msg.ack();
-    }
-}
