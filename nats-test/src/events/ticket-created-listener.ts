685ed526b66bf0ba3d630303b731a59614fa5f18
thorstenneff <thorstenneff@gmx.de>
2025-01-07 17:37:11 +0100

    Moved nats code to common

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -5,7 +5,7 @@ import { Subjects } from "./subjects";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     readonly subject = Subjects.TicketCreated;
-    queueGoupName = 'payments-service';
+    queueGroupName = 'payments-service';
 
     onMessage(data: TicketCreatedEvent['data'], msg: Message) {
         console.log('Event data', data);
