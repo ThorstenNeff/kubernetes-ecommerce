6b472185847852fe1c8f0103bfd767bec1d8340b
thorstenneff <thorstenneff@gmx.de>
2025-01-08 17:08:25 +0100

    Delete order route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotAuthorizedError, NotFoundError } from '@neffuke/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router(); 
 
-router.delete('api/orders/:orderId', async (req: Request, res: Response) => {
-    res.send({});
+router.delete('api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(req.params.orderId);
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+
+    await order.save();
+
+    // publish an event, that this was cancelled
+
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter }
