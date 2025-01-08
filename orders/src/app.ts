ad19242e3f4bfdd3a61888b46166ab51f4ba1865
thorstenneff <thorstenneff@gmx.de>
2025-01-08 13:28:34 +0100

    Duplicate ticket service -> order service

---
+++ orders/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@neffuke/common'
+import { createTicketRouter  } from './routes/new';
+import { showTicketRouter  } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express(); 
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    // Only set cookies in https, not http (but allow http for tests)
+    secure: process.env.NODE_ENV !== 'test'
+}));
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async () => {
+    console.log("In not found");
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app }
