e6a70bc27165b1f9ad5ac53a6394163cb08fa252
thorstenneff <thorstenneff@gmx.de>
2025-01-08 09:49:21 +0100

    Graceful shutdown for Nats server

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 
 import mongoose, { ConnectOptions } from 'mongoose';
 import { app } from './app'
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -12,6 +13,14 @@ const start = async () => {
     }
 
     try {
+        await natsWrapper.connect("gke_ticketing-dev-444518_europe-west1-b_ticketing-dev","qwertz", "http://nats-srv:4222");
+        natsWrapper.client.on('close', () => {
+            console.log("NATS connection closed");
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+
         await mongoose.connect(process.env.MONGO_URI,  {
         } as ConnectOptions);
         console.log("Connected to MongoDB");
