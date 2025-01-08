4edcff65191a8fdf2aa88bcb2f17bdaebfbef2d1
thorstenneff <thorstenneff@gmx.de>
2025-01-08 16:07:51 +0100

    Add isReserved method to ticket schema

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@neffuke/common';
+import { NotFoundError, requireAuth, validateRequest, OrderStatus, BadRequestError } from '@neffuke/common';
 import { body } from 'express-validator';
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router(); 
 
@@ -10,6 +12,34 @@ router.post('api/orders', requireAuth, [
     .isEmpty()
     .withMessage("TicketId must be provided")
 ], validateRequest, async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+        throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the order status is *not* cancelled
+    // If we find an order from that means
+    const existingOrder = await Order.findOne({
+        ticket: ticket, 
+        status: {
+            $in: [
+                OrderStatus.Created, 
+                OrderStatus.Complete, 
+                OrderStatus.AwaitingPayment
+            ]
+        }
+    });
+    if (existingOrder) {
+        throw new BadRequestError("Ticket is already reserved");
+    }
+
+    // Calculate an expiration date
+    
     res.send({});
 });
 
