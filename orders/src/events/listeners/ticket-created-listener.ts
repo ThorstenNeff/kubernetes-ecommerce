4bfc263c86b8900c3495009173c7d1ecbfb59580
thorstenneff <thorstenneff@gmx.de>
2025-01-09 14:42:10 +0100

    Publish

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -7,8 +7,8 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
     readonly subject = Subjects.TicketCreated;
     queueGoupName = queueGroupName;
 
-    async onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
-        const { id, title, price } = data;
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+        const { id,title, price } = data;
         const ticket = Ticket.build({
             id, title, price
         });
