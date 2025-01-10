147f2c9d86cf7fa86cf4d8138f929ed9be2d5777
thorstenneff <thorstenneff@gmx.de>
2025-01-10 12:52:03 +0100

    Setup payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@neffuke/common'
+
+const app = express(); 
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    // Only set cookies in https, not http (but allow http for tests)
+    secure: process.env.NODE_ENV !== 'test'
+}));
+app.use(currentUser);
+
+app.all('*', async () => {
+    console.log("In not found");
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app }
