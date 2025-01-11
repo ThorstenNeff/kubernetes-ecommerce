7700e1738d358a9533af48181bedfb23239298b4
thorstenneff <thorstenneff@gmx.de>
2025-01-11 14:30:32 +0100

    Ticket list

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,35 @@
-const LandingPage = ({currentUser}) => {
+const LandingPage = ({ currentUser, tickets }) => {
   console.log("Current user: ", currentUser);
-  if (currentUser) {
-    return <h1>You are signed in</h1>;
-  } else {
-    return <h1>You are not signed in</h1>;
-  }
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+  
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          { ticketList }
+        </tbody>
+      </table>
+    </div>
+  )
 };
   
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get('/api/tickets');
+  return { tickets:data };
 };
 
 export default LandingPage;
